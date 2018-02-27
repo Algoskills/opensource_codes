d4c268593066545a0b68051ff8ded9bb78a36668
Swapnil Sourabh <swapnil1108@gmail.com>
2018-02-27 19:49:25 +0530

    add indentation to bubble and bubbleadv

--- algorithm_codes/algoskills_bubble.cpp
+++ algorithm_codes/algoskills_bubble.cpp
@@ -9,11 +9,11 @@ int main()
 	int arr[n];
 	cout<<"Enter numbers: ";
 	for(i = 0;i< n; i++)// to input the numbers into an array
-	cin>>arr[i];
+		cin>>arr[i];
 	bubble(arr,n);
 	cout<<"Array sorted in increasing order: "<< endl;
 	for(i = 0;i< n; i++)
-	cout<< arr[i]<<" ";
+		cout<< arr[i]<<" ";
 	return 0;
 }
 
@@ -21,13 +21,13 @@ void bubble(int a[], int n) //Notice something special? read explanation below!
 {
 	int i,j,temp;
 	for(i = 0;i< n-1; i++) //for cycles
-	for (j = 0; j < (n-1)-i; j++) //for comparison
-	{
-		if( a[j] > a[j+1]) //use < for decreasing order
+		for (j = 0; j < (n-1)-i; j++) //for comparison
 		{
-			temp= a[j];//temp is used to swap/exchange a[j] and a[j+1]
-			a[j]= a[j+1];
-			a[j+1]=temp;
+			if( a[j] > a[j+1]) //use < for decreasing order
+			{
+				temp= a[j];//temp is used to swap/exchange a[j] and a[j+1]
+				a[j]= a[j+1];
+				a[j+1]=temp;
+			}
 		}
-	}
 }
